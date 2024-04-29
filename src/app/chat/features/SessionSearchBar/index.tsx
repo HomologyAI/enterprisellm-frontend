087564b6c7b97fdf6355e449b4541fd21e34466f
bb1998 <1215373008@qq.com>
2024-04-29 15:49:49 +0800

    feat: 去掉顶部，增加点辨识度

--- src/app/chat/features/SessionSearchBar/index.tsx
+++ src/app/chat/features/SessionSearchBar/index.tsx
@@ -1,14 +1,17 @@
-import { SearchBar } from '@lobehub/ui';
+import {ActionIcon, SearchBar} from '@lobehub/ui';
 import { memo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { useIsMobile } from '@/hooks/useIsMobile';
 import { useSessionStore } from '@/store/session';
+import {Flexbox} from "react-layout-kit";
+import {MessageSquarePlus} from "lucide-react";
+import {DESKTOP_HEADER_ICON_SIZE} from "@/const/layoutTokens";
 
 const SessionSearchBar = memo<{ mobile?: boolean }>(({ mobile: controlledMobile }) => {
   const { t } = useTranslation('chat');
   const [keywords, setKeywords] = useState<string | undefined>(undefined);
-  const [useSearchSessions] = useSessionStore((s) => [s.useSearchSessions]);
+  const [useSearchSessions, createSession] = useSessionStore((s) => [s.useSearchSessions, s.createSession]);
 
   useSearchSessions(keywords);
 
@@ -16,20 +19,29 @@ const SessionSearchBar = memo<{ mobile?: boolean }>(({ mobile: controlledMobile 
   const mobile = controlledMobile ?? isMobile;
 
   return (
-    <SearchBar
-      allowClear
-      enableShortKey={!mobile}
-      onChange={(e) => {
-        const newKeywords = e.target.value;
-        setKeywords(newKeywords);
-        useSessionStore.setState({ isSearching: !!newKeywords });
-      }}
-      placeholder={t('searchAgentPlaceholder')}
-      shortKey={'k'}
-      spotlight={!mobile}
-      type={mobile ? 'block' : 'ghost'}
-      value={keywords}
-    />
+    <Flexbox horizontal align={'space-between'} >
+      <SearchBar
+        allowClear
+        enableShortKey={!mobile}
+        onChange={(e) => {
+          const newKeywords = e.target.value;
+          setKeywords(newKeywords);
+          useSessionStore.setState({ isSearching: !!newKeywords });
+        }}
+        placeholder={t('searchAgentPlaceholder')}
+        shortKey={'k'}
+        spotlight={!mobile}
+        type={mobile ? 'block' : 'ghost'}
+        value={keywords}
+      />
+      <ActionIcon
+        icon={MessageSquarePlus}
+        onClick={() => createSession()}
+        size={DESKTOP_HEADER_ICON_SIZE}
+        style={{ flex: 'none' }}
+        title={t('newAgent')}
+      />
+    </Flexbox>
   );
 });
 
