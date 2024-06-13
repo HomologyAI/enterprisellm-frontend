aa4af9874e0d8d8634f3daab8901d226323a8763
Wang Da <atomwangda@gmail.com>
2024-06-14 05:03:07 +0800

    fix: 修改输入框最大字符限制为15000

--- src/app/chat/(desktop)/features/ChatInput/TextArea.tsx
+++ src/app/chat/(desktop)/features/ChatInput/TextArea.tsx
@@ -88,7 +88,7 @@ const InputArea = memo<InputAreaProps>(({ setExpand }) => {
         onPressEnter={(e) => {
           if (loading || e.altKey || e.shiftKey || isChineseInput.current) return;
 
-          if (value.length >= 400) {
+          if (value.length >= 15_000) {
             message.warning('提问内容超过最大长度限制，请修改提问内容。')
             return
           }
