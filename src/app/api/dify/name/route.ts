53063272badfd9426cc645ad3443fb2c1974c520
Jin Shang <shangjin1997@gmail.com>
2024-07-11 13:41:52 +0800

    fix getname

--- src/app/api/dify/name/route.ts
+++ src/app/api/dify/name/route.ts
@@ -29,10 +29,10 @@ export async function POST(req: NextRequest) {
     return createErrorResponse(ChatErrorType.InternalServerError, body);
   }
 
-  if (app?.apiKey) {
-    chatClient.updateApiKey(app.apiKey);
+  if (app?.appKey) {
+    chatClient.updateApiKey(app.appKey);
   }
-
+  console.log(chatClient)
   return chatClient.getConversationName(
     conversation_id,
     userId,
